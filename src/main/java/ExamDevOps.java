bea0d4db62f7ef7671ca10bd170188f1b7a6f7a9
S26poste07\IPI <haddou.0014@gmail.com>
2018-11-09 15:30:08 +0100

    dfg
    sdgf

---
+++ src/main/java/ExamDevOps.java
@@ -0,0 +1,9 @@
+
+public class ExamDevOps {
+	
+		public static void main(String[] args) {
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("Hello, dfjjdhj,ghjghjghjfg  ");
+	    }
+	
+}
